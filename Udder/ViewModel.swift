d8bd0b2d85aa614c534a980f0895a772f20c950b
lukskoki <luka.kovacevic141@gmail.com>
2024-03-27 07:51:24 +0100

    add ViewModel

---
+++ Udder/ViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  ViewModel.swift
+//  Udder
+//
+//  Created by dujo kovacevic on 27.03.2024..
+//
+
+import SwiftUI
+
+struct ViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ViewModel()
+}
