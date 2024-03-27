8cc052f40d3b2e786fd169d7ec3a81ba2bd1430b
lukskoki <luka.kovacevic141@gmail.com>
2024-03-27 07:19:06 +0100

    Initial Commit

---
+++ Udder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Udder
+//
+//  Created by dujo kovacevic on 27.03.2024..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by dujo kovacevic on 27.03.2024..
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
